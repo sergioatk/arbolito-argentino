d7ea02029ded685351239c1d74f3fc88b6e1ae5a
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-08-23 17:55:08 -0300

    crea modulos js para empezar a trabajar con la api

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+import {buscarEnAPI} from '/modulos/api/buscarEnAPI.js'
+import {mostrarCotizacion} from '/modulos/ui/ui.js'
+
+
+
+buscarEnAPI();
---
+++ src/modulos/ui/ui.js
@@ -0,0 +1,3 @@
+export function mostrarCotizacion(cotizacion) {
+
+}
