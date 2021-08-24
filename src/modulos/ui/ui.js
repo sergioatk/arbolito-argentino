9015d480476c18150c5be2203dd1390cd12fff3a
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-08-24 11:10:55 -0300

    configura funcion para mostrar resultados

--- src/modulos/ui/ui.js
+++ src/modulos/ui/ui.js
@@ -1,3 +1,41 @@
-export function mostrarCotizacion(cotizacion) {
+export async function mostrarCotizacion(APIcallback) {
+    const nuevaCotizacion = await APIcallback();
 
+    const $cuadrosDolarOficial = document.querySelectorAll('.dolar-oficial');
+    const $cuadrosDolarBlue = document.querySelectorAll('.dolar-blue');
+    const $cuadrosEuroOficial = document.querySelectorAll('.euro-oficial');
+    const $cuadrosEuroBlue = document.querySelectorAll('.euro-blue');
+
+    // Renderizar Fecha
+    const $fecha = document.querySelector('#fecha');
+    $fecha.textContent += nuevaCotizacion.fecha.split('T')[0];
+    // Renderizar Cotizacion Dolar Oficial
+    $cuadrosDolarOficial.forEach((cuadro, i) => {
+        const $nuevoValor = document.createElement('span');
+        $nuevoValor.textContent = nuevaCotizacion.dolarOficial[i]
+        cuadro.appendChild($nuevoValor)
+    })
+
+    // Renderizar Cotizacion Dolar Blue
+    $cuadrosDolarBlue.forEach((cuadro, i) => {
+        const $nuevoValor = document.createElement('span');
+        $nuevoValor.textContent = nuevaCotizacion.dolarBlue[i]
+        cuadro.appendChild($nuevoValor)
+    })
+
+    // Renderizar Cotizacion Euro Oficial
+    $cuadrosEuroOficial.forEach((cuadro, i) => {
+        const $nuevoValor = document.createElement('span');
+        $nuevoValor.textContent = nuevaCotizacion.euroOficial[i]
+        cuadro.appendChild($nuevoValor)
+    })
+
+    // Renderizar Cotizacion Euro Blue
+    $cuadrosEuroBlue.forEach((cuadro, i) => {
+        const $nuevoValor = document.createElement('span');
+        $nuevoValor.textContent = nuevaCotizacion.euroBlue[i]
+        cuadro.appendChild($nuevoValor)
+    })
+
+    
 }
