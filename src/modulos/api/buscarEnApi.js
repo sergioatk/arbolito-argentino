fcc04c8150d68626afbef6551e081f8e729377ff
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-08-24 11:11:17 -0300

    actualiza funcion para llamar api

--- src/modulos/api/buscarEnApi.js
+++ src/modulos/api/buscarEnApi.js
@@ -2,10 +2,10 @@ import {Cotizacion} from '/modulos/clases/Cotizacion.js'
 
 export async function buscarEnAPI() {
     const cotizaciones = await fetch('https://api.bluelytics.com.ar/v2/latest');
-    const respuesta = cotizaciones.json();
+    const respuesta = await cotizaciones.json();
 
     const { oficial: dolarOficial, blue: dolarBlue, oficial_euro: euroOficial, blue_euro: euroBlue, last_update: fecha } = respuesta;
-
+    
     const nuevaCotizacion = new Cotizacion(dolarOficial, dolarBlue, euroOficial, euroBlue, fecha)
     
     return nuevaCotizacion;
